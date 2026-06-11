ceb430281c7928a891e63f25d5d1d46fcfc7339d
Diego Menezes <diegopmenezes@hotmail.com>
2026-06-11 09:39:37 -0300

    feat: enable production order persistence

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  persistOrders: false,
+  supabaseUrl: 'https://yewqtzropmosgopwvxlb.supabase.co',
+  supabaseAnonKey: 'sb_publishable_tCAWmp6tivvtQA7qrxcjbw_w8hACwOr',
+  persistOrders: true,
 };
